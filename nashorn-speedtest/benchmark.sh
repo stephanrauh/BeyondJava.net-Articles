5e0b13e517d6c9dd855ff32b26f1cfc78ef6d420
stephan <webmaster1@beyondjava.de>
2021-03-17 20:35:27 +0100

    updated benchmark results

--- nashorn-speedtest/benchmark.sh
+++ nashorn-speedtest/benchmark.sh
@@ -1,32 +1,50 @@
 
 echo "Native node.js"
-node --version
-node ./src/main/javascript/speedtest.js
+node --version > results-native-node.js-14.16.0.txt
+node ./src/main/javascript/speedtest.js > results-native-node.js-14.16.0.txt
 
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home
 export PATH=$JAVA_HOME/bin:$PATH
-java -version
+java -version > results-GraalVM-CE-21.0.0.2.txt
 
 mvn clean install
 
-echo "GraalVM 20.1.0, Java 11.0.7:"
+echo "GraalVM CE 21.0.0.2, Java 11.0.10:"
 
-java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar > results-GraalVM-CE-21.0.0.2.txt
 
-echo "AdoptOpenJDK 11.0.5:"
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.0.0.2/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
+java -version > results-GraalVM-EE-21.0.0.2.txt
+
+mvn clean install
+
+echo "GraalVM EE 21.0.0.2, Java 11.0.10:"
+
+java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar > results-GraalVM-EE-21.0.0.2.txt
+
+
+echo "AdoptOpenJDK 11.0.10+9:"
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home 
 export PATH=$JAVA_HOME/bin:$PATH
-java -version
+java -version > results-AdoptOpenJDK-11.0.10+9.txt
 
-java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar > results-AdoptOpenJDK-11.0.10+9.txt
 
-echo "AdoptOpenJDK 14.0.5:"
+echo "AdoptOpenJDK 11.0.10 J9:"
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11-openj9.jdk/Contents/Home 
 export PATH=$JAVA_HOME/bin:$PATH
-java -version
+java -version > results-AdoptOpenJDK-11.0.10-with-J9-compiler.txt
 
-java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar > results-AdoptOpenJDK-11.0.10-with-J9-compiler.txt
 
+echo "AdoptOpenJDK 16:"
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-16.jdk/Contents/Home 
+export PATH=$JAVA_HOME/bin:$PATH
+java -version > results-AdoptOpenJDK-16+36.txt
+
+java -jar ./target/rhino-nashorn-graalvm-speedtest-1.0-SNAPSHOT-jar-with-dependencies.jar > results-AdoptOpenJDK-16+36.txt
